6c3f11eb3f8def8c4e2fc89d8a284ee06d44ac98
Binny <ibinstock@outlook.com>
2022-11-27 15:02:31 -0800

    set up email regex validation within User model

--- models/User.js
+++ models/User.js
@@ -1,22 +1,35 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Scheme (
+const userSchema = new Schema (
     {
         username: {
-
+            type: String,
+            unique: true,
+            required: [true, 'Username required'],
+            trim: true
         },
         email: {
-
+            type: String,
+            unique: true,
+            required: [true, 'Email required'],
+            validate: {
+                validator: function(v) {
+                    return /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+                },
+                message: props => `${props.value} is not a valid email`
+            }
         },
         thoughts: [
             {
                 // references the Thought ObjectId
             }
+            // Array of _id values referencing the Thought model
         ],
         friends: [
             {
                 // references the User ObjectId
             }
+            // Array of _id values referencing the User model (self-reference)
         ]
     },
     {
