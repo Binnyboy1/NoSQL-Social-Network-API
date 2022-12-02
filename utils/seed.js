99f50123f29cd569d1e915f24c6c4adef8020e50
Binny <ibinstock@outlook.com>
2022-12-01 18:23:42 -0800

    added 'dateformat' as an npm dependency in package.json

--- utils/seed.js
+++ utils/seed.js
@@ -1,4 +1,4 @@
-const connection = require('../config/connection');
+const connection = require('../config/connections');
 const { User, Thought } = require('../models');
 
 // Start the seeding runtime timer
